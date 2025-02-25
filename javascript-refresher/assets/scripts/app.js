623d71fb0984b2375ae18f46d849a01caa81faa7
Arin <arin.apichitchon@gmail.com>
2025-02-24 22:25:14 -0500

    Objects

--- javascript-refresher/assets/scripts/app.js
+++ javascript-refresher/assets/scripts/app.js
@@ -1,4 +1,26 @@
-export default (userName) => {
-    console.log("Hello");
-    return userName;
+const user = {
+    name: "Arin",
+    age: 30,
+    // method
+    greet() {
+        console.log("Hello " + this.name);
+    }
 };
+
+console.log(user);
+console.log("age is: " + user.age);
+user.greet();
+
+class User {
+    constructor(nanme, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet() {
+        console.log("Hi");
+    }
+}
+
+const user1 = new User("John", 25);
+console.log(user1);
+user1.greet();
